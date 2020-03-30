6e5fea18950fa5a71a5f7c0cf6d7c6c61daf24d1
NikitaKemarskiy <sashapobeditel1980@gmail.com>
2020-03-30 14:40:14 +0300

    .csv parser was updated. If column value is surrounded with quote chars - it removes them.

--- helpers.lisp
+++ helpers.lisp
@@ -2,19 +2,52 @@
 	(setq match (mismatch str2 str1 :from-end t))
 	(or (not match) (= 0 match)))
 
-(defun split-row (row separator quote-char &optional (result '()) (index 0))
+(defun split-row (row separator quote-char &optional (result '()) (column-start-index 0))
+"Splits row into list of columns values
+(we pass separator and quote char as params)."
 	(setq
 		column-end-index
 			(cond
-				((= index (length row)) nil)
-				(t (position separator row :test #'equal :start index)))
-		column (subseq row index column-end-index))
+				((= column-start-index (length row)) nil)
+				(t (get-column-end-index row separator quote-char column-start-index)))
+		column
+			(parse-column
+				(subseq row column-start-index column-end-index)
+				quote-char)
+		result-appended
+			(append
+				result
+				(list column)))
 	(cond
 		(column-end-index
 			(split-row
 				row
 				separator
 				quote-char
-				(append result (list column))
+				result-appended
 				(+ column-end-index 1)))
-		(t result)))
+		(t result-appended)))
+
+(defun parse-column (column quote-char)
+"If column isn't surrounded with quote chars - return column.
+Otherwise quote chars will be removed, doubled quote chars inside
+will be replaced with only one."
+	(cond
+		((and (> (length column) 1) (char= (char column 0) quote-char))
+			(subseq column 1 (- (length column) 1)))
+		(t column)))
+
+(defun get-column-end-index (row separator quote-char column-start-index)
+	(cond
+		((char= (char row column-start-index) quote-char)
+			(setq
+				closing-quote-char-index
+				(get-closing-quote-char-index
+					row
+					quote-char
+					(+ column-start-index 1)))
+			(+ closing-quote-char-index 1))
+		(t (position separator row :test #'equal :start column-start-index))))
+
+(defun get-closing-quote-char-index (row quote-char opening-quote-char-index)
+	(position quote-char row :test #'equal :start opening-quote-char-index))
